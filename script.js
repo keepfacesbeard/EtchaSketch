fd42607ef79ccd7458da8084eb5c654ffc1114db
keepfacesbeard <criendeau143@gmail.com>
2021-12-06 14:54:08 -0800

    works great, could use some styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sketchzone = document.getElementsByClassName('sketchzone')[0];
-
+let color = "black"
+let rainbow = false;
 console.log(sketchzone)
 
 for (let i=1; i<=10000; i++) {
@@ -13,9 +14,48 @@ for (let i=1; i<=10000; i++) {
 let sqrs = document.querySelector('.sketchzone');
 
 sqrs.addEventListener('mouseover', function(event){
-    let sqr = event.target;
-    sqr.style.backgroundColor = "red";
+    
+    if (event.target.classList.contains("squares")) {
+        let sqr = event.target;
+        if (rainbow == false) {
+            sqr.style.backgroundColor = `${color}`;
+        }
+        else if (rainbow == true) {
+            sqr.style.backgroundColor = multiColor();     
+        }
+    }
 });
 
 
+function resetSketch() {
+    for (let i=1; i<=10000; i++) {
+        let resetsqr = document.querySelector(`.square${i}`);
+        resetsqr.style.backgroundColor = 'white';
+        rainbow = false;
+    }
+}
 
+const rainbowbutton = document.getElementById('rainbow');
+disengageRainbow();
+
+function engageRainbow() {
+    rainbow = true;
+    rainbowbutton.textContent="Turn Off Rainbow";
+    rainbowbutton.removeEventListener('click', engageRainbow);
+    rainbowbutton.addEventListener('click', disengageRainbow);
+}
+function disengageRainbow() {
+    rainbow = false;
+    color = 'black';
+    rainbowbutton.textContent="Turn On Rainbow";
+    rainbowbutton.removeEventListener('click', disengageRainbow);
+    rainbowbutton.addEventListener('click', engageRainbow);
+}
+
+function multiColor(){
+    let randoR = (Math.random()*255);
+    let randoG = (Math.random()*255);
+    let randoB = (Math.random()*255);
+    color = `rgb(${randoR}, ${randoG}, ${randoB})`;
+    return color;
+}
